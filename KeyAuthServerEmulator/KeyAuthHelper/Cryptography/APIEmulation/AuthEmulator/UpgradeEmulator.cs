408577aa60aea803746ae79af77d5b50b46ef224
Cabbo <92642446+CabboShiba@users.noreply.github.com>
2022-12-30 21:01:17 +0100

    Add files via upload

---
+++ KeyAuthServerEmulator/KeyAuthHelper/Cryptography/APIEmulation/AuthEmulator/UpgradeEmulator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyAuthServerEmulator.KeyAuthHelper.Cryptography.CustomPayLoad;
+
+/**
+ * FULL CREDITS TO HTTPS://GITHUB.COM/CABBOSHIBA
+ * DISCORD: FreeCabbo11#9191
+ * TELEGRAM: HTTPS://T.ME/CABBOSHIBA
+ * 
+ * If you do not get an answer on Discord for 48 Hours, this means I got banned. Contact me on Telegram.
+ */
+
+namespace KeyAuthServerEmulator.KeyAuthHelper.Cryptography.AuthEmulator
+{
+    internal class UpgradeEmulator
+    {
+        public static string Emulator(string Request)
+        {
+            string[] SplitContent = Request.Split('&');
+            string EncryptedResponse = null;
+            string InitIV = null;
+            foreach (var k in SplitContent)
+            {
+                if (k.StartsWith("init_iv="))
+                {
+                    string[] init = k.Split('=');
+                    InitIV = init[1];
+                    EncryptedResponse = Cryptography.encrypt(PayLoad.Upgrade, Credentials.EncKey, InitIV);
+                }
+
+            }
+            Logger.SaveLog($"KeyAuthRegister: {PayLoad.Upgrade}");
+            return EncryptedResponse;
+        }
+    }
+}
---
+++ KeyAuthServerEmulator/bin/Debug/ReadMe.txt
@@ -0,0 +1,3 @@
+If you already have the application's secret, place it in KeyAuthSecret.txt
+
+If you do not know the application's secret, grab the application strings and place them in Strings.txt.
